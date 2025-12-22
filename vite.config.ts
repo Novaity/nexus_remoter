4f9759232a5ae8f86b274e9d7e48aa5f961c0109
Novaity <efecancengiz1234@gmail.com>
2025-12-22 23:23:10 +0300

    fix: use relative paths for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: './', 
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
   build: {
     outDir: 'dist',
